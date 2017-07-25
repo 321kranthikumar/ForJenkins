dc1491c344d42ee8a005637f1bf3daef9125e562
venkatesh Pittala <venkirowling@gmail.com>
2017-07-25 00:31:04 -0400

    uu

--- HelloWorld/src/Helloworld.java
+++ HelloWorld/src/Helloworld.java
@@ -4,7 +4,7 @@ public class Helloworld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println(" MyHelloWorld");
-System.out.println(" HellovenkiS");
+System.out.println(" Hellovenkilu");
 	}
 
 }
