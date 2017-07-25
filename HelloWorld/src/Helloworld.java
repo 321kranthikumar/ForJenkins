0f5288d1f59467fbfdfe5c6bba16675a5d004de2
venkatesh Pittala <venkirowling@gmail.com>
2017-07-25 14:18:19 -0400

    "My recent Commit "

--- HelloWorld/src/Helloworld.java
+++ HelloWorld/src/Helloworld.java
@@ -4,7 +4,8 @@ public class Helloworld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println(" MyHelloWorld");
-System.out.println(" Hellovenkilu");
+System.out.println(" Hellovenkis");
+System.out.println("JANASENA");
 	}
 
 }
