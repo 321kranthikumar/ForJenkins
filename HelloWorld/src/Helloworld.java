c1f4f6333d35810ff369f9418416f816d8bf2558
venkatesh Pittala <venkirowling@gmail.com>
2017-07-24 22:48:36 -0400

    " commiting to the remote "

--- HelloWorld/src/Helloworld.java
+++ HelloWorld/src/Helloworld.java
@@ -4,7 +4,7 @@ public class Helloworld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println(" MyHelloWorld");
-System.out.println(" Hellovenki");
+System.out.println(" HellovenkiS");
 	}
 
 }
