4fc4b1eb798eae796a6304ea49595bfb51ad0dc0
venkatesh Pittala <venkirowling@gmail.com>
2017-07-25 15:45:04 -0400

    " to avoid the merge conflicts"

--- HelloWorld/src/Helloworld.java
+++ HelloWorld/src/Helloworld.java
@@ -4,7 +4,8 @@ public class Helloworld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println(" MyHelloWorld");
-System.out.println(" Hellovenkis");
+System.out.println(" Hellovenki");
+System.out.println(" HellovenkiS");
 System.out.println("JANASENA");
 	}
 
